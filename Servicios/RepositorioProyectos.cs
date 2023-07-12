cc81e0aa36e24440978ebb6ceea5068a0e80f08d
DanSou1 <125513682+DanSou1@users.noreply.github.com>
2023-07-12 01:24:53 -0500

    Initial commit

---
+++ Servicios/RepositorioProyectos.cs
@@ -0,0 +1,41 @@
+﻿using Portafolio.Models;
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<ProyectoDTO> obtenerProyectos()
+        {
+            return new List<ProyectoDTO>()
+            {
+                new ProyectoDTO
+                {
+                    Titulo =  "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP .NET Core",
+                    Link = "Google.com",
+                    ImagenURL = "FF"
+                },
+                 new ProyectoDTO
+                {
+                    Titulo =  "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP .NET Core",
+                    Link = "Google.com",
+                    ImagenURL = "FF"
+                },
+                  new ProyectoDTO
+                {
+                    Titulo =  "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP .NET Core",
+                    Link = "Google.com",
+                    ImagenURL = "FF"
+                },
+                   new ProyectoDTO
+                {
+                    Titulo =  "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP .NET Core",
+                    Link = "Google.com",
+                    ImagenURL = " DF"
+                },
+            };
+        }
+    }
+}
